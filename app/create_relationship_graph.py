7821e9356872b455dd115f9a5bb38e57ecd0b3a2
klarizsofi klarizsofi <klarizsofi@gmail.com>
2025-02-13 18:23:50 +0000

    add comments

--- app/create_relationship_graph.py
+++ app/create_relationship_graph.py
@@ -5,21 +5,24 @@ import numpy as np
 
 def create_graph(data: TrajectoryCollection):
     logging.info("Create relationship graphs")
-    all_z_coord_missing = data.trajectories[0].df.geometry.z.isnull().all()
-    if all_z_coord_missing:
-        # force2d oly works for geoseries..?
-        trajectory_a = data.trajectories[0].df.force_2d()
-        trajectory_b = data.trajectories[1].df.force_2d()
-        distance = trajectory_a.distance(trajectory_b)
-        print(distance)
 
+    trajectory_a = data.trajectories[0].df
+    trajectory_b = data.trajectories[1].df
 
-        # check if there is a 3rd dimension
-        # for each pair:
-        # get geoseries for 2 trajectories
-        # use force_2d()
-        # use a.distance(b) to get distances between points
-        # parameterize and define close - for how long and what distance?
-        # find distance segments that meet criteria
-        # get number of times the animals met and store
-        # make graph
+    # 1: interpolate and resample
+    # round
+    # remove extras (or avg)
+    # interpolate / fill with none depending on gap size
+
+    # 2: match series
+
+    # 3: if it's within params, calculate distance
+    # create now df with times they were both sampled with distance
+
+    # calculate distance
+    distance = trajectory_a.distance(trajectory_b)
+
+    # parameterize and define close - for how long and what distance?
+    # find distance segments that meet criteria
+    # get number of times the animals met and store
+    # make graph
