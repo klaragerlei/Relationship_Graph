cb8fbca7a5fe21b173fc2c576dc434ea94c6a556
klarizsofi klarizsofi <klarizsofi@gmail.com>
2025-02-13 18:38:22 +0000

    more notes

--- app/create_relationship_graph.py
+++ app/create_relationship_graph.py
@@ -10,19 +10,18 @@ def create_graph(data: TrajectoryCollection):
     trajectory_b = data.trajectories[1].df
 
     # 1: interpolate and resample
-    # round
-    # remove extras (or avg)
-    # interpolate / fill with none depending on gap size
+    # round (based on param)
+    # remove extras (or avg) - there might be multiple identical values after rounding
+    # interpolate values / or fill with none depending on gap size if the data is too sparse
+    # should return evenly sampled geoseries
 
     # 2: match series
+    # run (1) for both trajectories that are compared and cut off beginnings and ends, so they fully match
+    # 3: calculate distance
 
-    # 3: if it's within params, calculate distance
-    # create now df with times they were both sampled with distance
-
-    # calculate distance
     distance = trajectory_a.distance(trajectory_b)
 
-    # parameterize and define close - for how long and what distance?
+    # parameterize and define 'close' - for how long and what distance should they be to be considered interacting?
     # find distance segments that meet criteria
     # get number of times the animals met and store
     # make graph
