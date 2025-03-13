71b7a218d25b58cfaea9b68d4e871158a8838d52
klarizsofi klarizsofi <klarizsofi@gmail.com>
2025-03-13 17:07:54 +0000

    inteprolate

--- app/create_relationship_graph.py
+++ app/create_relationship_graph.py
@@ -1,5 +1,6 @@
 import logging
 from movingpandas import TrajectoryCollection
+import geopandas as gpd
 import numpy as np
 
 from datetime import datetime
@@ -10,6 +11,14 @@ import pandas as pd
 
 def round_time(dates: datetime, unit='H'):
     return dates.floor(unit)
+
+
+def interpolate_points(trajectory: gpd.GeoDataFrame, unit='H'):
+    # make new df with time, x and y coord
+    trajectory_df = pd.DataFrame({'x': trajectory.geometry.x, 'y': trajectory.geometry.y}, index=trajectory.index)
+    # use pandas interpolate
+    trajectory_df = trajectory_df.resample(unit).mean()   # this has nans in gaps for now
+    return trajectory_df
 
 
 def create_graph(data: TrajectoryCollection):
@@ -31,6 +40,8 @@ def create_graph(data: TrajectoryCollection):
     times_b_unique = trajectory_b[~trajectory_b.index.duplicated(keep='first')]
 
     # interpolate values / or fill with none depending on gap size if the data is too sparse
+    times_a_interpolated = interpolate_points(times_a_unique)
+    times_b_interpolated = interpolate_points(times_b_unique)
 
 
     # should return evenly sampled geoseries
