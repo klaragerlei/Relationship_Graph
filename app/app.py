4df5746fe09d3a60e4263de9ba3a50269a4bd604
Klara Gerlei <klarizsofi@gmail.com>
2025-12-15 17:26:50 +0000

    change returned df

--- app/app.py
+++ app/app.py
@@ -2,12 +2,8 @@ from sdk.moveapps_spec import hook_impl
 from sdk.moveapps_io import MoveAppsIo
 from movingpandas import TrajectoryCollection
 import logging
-import matplotlib.pyplot as plt
 
-# showcase for importing functions from another .py file (in this case from "./app/getGeoDataFrame.py")
-from app.getGeoDataFrame import get_GDF
 from app.create_relationship_graph import create_graph
-
 
 
 class App(object):
@@ -17,46 +13,10 @@ class App(object):
 
     @hook_impl
     def execute(self, data: TrajectoryCollection, config: dict) -> TrajectoryCollection:
-
         logging.info(f'Welcome to the {config}')
 
-        """Your app code goes here"""
+        # Create the relationship graph (your main app logic)
         create_graph(data.copy())
 
-        # showcase injecting App settings (parameter `year`)
-        data_gdf = get_GDF(data)  # translate the TrajectoryCollection to a GeoDataFrame
-
-
-
-        logging.info(f'Subsetting data for {config["year"]}')
-        # subset the data to only contain the specified year
-        if config["year"] in data_gdf.index.year:
-            result = data_gdf[data_gdf.index.year == config["year"]]
-        else:
-            result = None
-
-        # showcase creating an artifact
-        if result is not None:
-            result.plot(column=data.get_traj_id_col(), alpha=0.5)
-            plot_file = self.moveapps_io.create_artifacts_file("plot.png")
-            plt.savefig(plot_file)
-            logging.info(f'saved plot to {plot_file}')
-        else:
-            logging.warning("Nothing to plot")
-
-        # showcase accessing auxiliary files
-        auxiliary_file_a = MoveAppsIo.get_auxiliary_file_path("auxiliary-file-a")
-        with open(auxiliary_file_a, 'r') as f:
-            logging.info(f.read())
-
-        # Translate the result back to a TrajectoryCollection
-        if result is not None:
-            result = TrajectoryCollection(
-                result,
-                traj_id_col=data.get_traj_id_col(),
-                t=data.to_point_gdf().index.name,
-                crs=data.get_crs()
-            )
-
-        # return the resulting data for next Apps in the Workflow
-        return None
+        # Return the original data unchanged for the next app in the workflow
+        return data
